b7856479ba72af10ad781eb742eb7c91f7e3ae9b
mmtzitziras <mariostzitziras4@gmail.com>
2024-05-16 17:53:28 +0300

    Set up camera and video call

--- src/VideoCall.jsx
+++ src/VideoCall.jsx
@@ -1,13 +1,37 @@
 /* eslint-disable no-unused-vars */
 import './VideoCall.css'
-import React from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import ReactDOM from 'react-dom/client'
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
 
 
 export default function VideoCall(){
+
+    const videoRef = useRef(null);
+    
+    const getVideo = () => {
+        navigator.mediaDevices
+            .getUserMedia({video:{width: 1920, height:1080}
+            })
+            .then(stream => {
+                let video = videoRef.current;
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(err => {
+                console.error(err);
+            })
+
+    }
+
+    useEffect(() => {
+        getVideo();
+    }, [videoRef])
+
     return (
         <div className="video-call">
-            sads
+            <video ref={videoRef}></video>
         </div>
     );
 }
